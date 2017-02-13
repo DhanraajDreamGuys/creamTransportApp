9da513078ae34c3b36f6201c21e01d52948d90f3
User <dhanraaj.s@dreamguys.co.in>
2017-02-13 16:18:24 +0530

    validation,toolbar is updated

--- app/src/main/java/co/in/dreamguys/cream/Login.java
+++ app/src/main/java/co/in/dreamguys/cream/Login.java
@@ -4,29 +4,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import co.in.dreamguys.cream.utils.Util;
 
 /**
  * Created by user5 on 09-02-2017.
  */
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
 
-    CustomAdapter mCustomAdapter;
+    /*CustomAdapter mCustomAdapter;
     ListView mListView;
     Button mSearch;
+    */
+
+    EditText email, password;
+    Button login;
+    TextView title;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_login);
 
-        mListView = (ListView) findViewById(R.id.lv_search_items);
+        initviews();
+
+
+
+
+
+
+      /*  mListView = (ListView) findViewById(R.id.lv_search_items);
         mSearch = (Button) findViewById(R.id.bt_search);
         mCustomAdapter = new CustomAdapter();
         mListView.setAdapter(mCustomAdapter);
@@ -36,11 +57,70 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(Login.this, Company.class));
             }
-        });
+        });*/
 
     }
 
-    private class CustomAdapter extends BaseAdapter {
+
+    public void initviews() {
+
+        email = (EditText) findViewById(R.id.ALET_email);
+        password = (EditText) findViewById(R.id.ALET_password);
+        login = (Button) findViewById(R.id.ALBT_signin);
+        title = (TextView) findViewById(R.id.ALTV_title);
+        login.setOnClickListener(this);
+        title.setText(getString(R.string.toolbar_title));
+        mToolbar = (Toolbar) findViewById(R.id.ATTB_toolbar);
+        setSupportActionBar(mToolbar);
+
+        mToolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_back));
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.ALBT_signin) {
+
+
+            if (email.getText().toString().isEmpty() && password.getText().toString().isEmpty()) {
+                email.setError(getString(R.string.err_email));
+                password.setError(getString(R.string.err_password));
+                email.requestFocus();
+                password.requestFocus();
+            } else if (email.getText().toString().isEmpty()) {
+                email.setError(getString(R.string.err_email));
+                email.requestFocus();
+            } else if (!Util.isValidEmail(email.getText().toString())) {
+                email.setError(getString(R.string.err_emailInvalid));
+                email.requestFocus();
+            } else if (password.getText().toString().isEmpty()) {
+                password.setError(getString(R.string.err_password));
+                password.requestFocus();
+            } else if (password.getText().toString().length() < 6) {
+                password.setError(getString(R.string.err_password_length));
+                password.requestFocus();
+            } else if (!Util.isNetworkAvailable(this)) {
+                Toast.makeText(Login.this, getString(R.string.no_internet_connection), Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+    }
+
+   /* private class CustomAdapter extends BaseAdapter {
 
 
         LayoutInflater mInflater;
@@ -67,5 +147,5 @@ public class Login extends AppCompatActivity {
             }
             return convertView;
         }
-    }
+    }*/
 }
