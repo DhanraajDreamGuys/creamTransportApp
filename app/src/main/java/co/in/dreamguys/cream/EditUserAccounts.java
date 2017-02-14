a2e093e9b8ddedd6e3f5fcace151c632a1f53233
User <dhanraaj.s@dreamguys.co.in>
2017-02-14 16:53:15 +0530

    Edituseraccounts class created,users class,actvity_account layout updated. user_acc_details,user_acc_edit_details,view_accident_report layout created. strings file updated android manifest updated

---
+++ app/src/main/java/co/in/dreamguys/cream/EditUserAccounts.java
@@ -0,0 +1,21 @@
+package co.in.dreamguys.cream;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import co.in.dreamguys.cream.utils.CustomProgressDialog;
+
+/**
+ * Created by User on 2/14/2017.
+ */
+public class EditUserAccounts extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_acc_edit_details);
+
+    }
+}
--- app/src/main/java/co/in/dreamguys/cream/Users.java
+++ app/src/main/java/co/in/dreamguys/cream/Users.java
@@ -1,18 +1,38 @@
 package co.in.dreamguys.cream;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by user5 on 14-02-2017.
  */
 
-public class Users extends AppCompatActivity {
+public class Users extends AppCompatActivity implements View.OnClickListener {
+
+    Button update;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_users);
+        setContentView(R.layout.activity_view_accident_report);
+
+     /*   initviews();*/
+
+    }
+
+   /* public void initviews(){
+
+        update = (Button) findViewById(R.id.AUAD_BT_update);
+        update.setOnClickListener(this);
+    }*/
+
+
+    @Override
+    public void onClick(View v) {
+      /*  startActivity(new Intent(Users.this, EditUserAccounts.class));*/
     }
 }
